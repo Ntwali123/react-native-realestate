222626a5740a222dbf33aaed1c4ad3c9d7f00183
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2024-12-21 12:38:29 +0200

    setting up fonts and assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { Image, StyleSheet, View, Text } from 'react-native';
 import { Link } from 'expo-router';
 
-export default function HomeScreen() {
+export default function Index() {
   return (
     <View
       style={{
@@ -10,6 +10,7 @@ export default function HomeScreen() {
         alignItems: "center",
       }}
     >
+      <Text className='font-bold text-green-500 text-lg my-10 font-rubik-bold'>Welcome to ReState</Text>
       <Link href="/sign-in">Sign in</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
