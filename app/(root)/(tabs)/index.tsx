ba5fab019383c32d5d2175d6e3ea3ec16924954c
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2024-12-21 09:25:52 +0200

    seting up routes

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Image, StyleSheet, View, Text } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const signIn = () => {
+  return (
+    <View>
+        <Text>Sign In</Text>
+    </View>
+  )
+}
+
+export default signIn
